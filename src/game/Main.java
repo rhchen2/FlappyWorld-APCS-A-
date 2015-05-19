4141006735b30fc01ce353a13e3fcaa2483e35ac
ronaksheth97 <ronaksheth97@gmail.com>
2015-05-19 14:32:44 -0700

    Updated to Gemma's Physics
    Copied previous code from past labs into project for further editing

--- src/game/Main.java
+++ src/game/Main.java
@@ -1,5 +1,7 @@
 package game;
 import javafx.animation.Interpolator;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
 import javafx.animation.Timeline;
 import javafx.animation.TranslateTransition;
 import javafx.application.Application;
@@ -11,6 +13,8 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
@@ -22,38 +26,85 @@ public class Main extends Application{
 	private ImageView bkgrd = null ;
 	private Node flappy = null;
 	
-    private void addActionEventHandler(){
+	private void addActionEventHandler(){
         button.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-            	//TODO: start the drop animation of the bird
-            	 
-            	TranslateTransition transTransition = new TranslateTransition(Duration.millis(2000), flappy);
-                transTransition.setToY(400);
-                transTransition.setInterpolator(new Interpolator() {
-    				protected double curve(double t) {
-    					double a = 1.2;
-    					return a * t *t + (1-a)*t ;
-    				}
-                   });    
-                transTransition.setCycleCount(Timeline.INDEFINITE);
-                transTransition.play();
-	
+                //TODO: start the drop animation of the bird
+               
+                Timeline tl = new Timeline();
+                tl.setCycleCount(1);
+                KeyValue kv = new KeyValue(flappy.translateYProperty(), 1000, new Interpolator() {
+                        @Override
+                                protected double curve(double t) {
+                                return 1.5 * t * t;
+                                }
+                });
+                KeyFrame drop = new KeyFrame(new Duration(2750), kv);
+                tl.getKeyFrames().add(drop);
+                tl.setAutoReverse(false);
+                tl.play();
+               
+                /*
+                TranslateTransition trans = new TranslateTransition(new Duration(3000), flappy);
+                trans.setToY(1000);
+                trans.setCycleCount(1);                
+                trans.setInterpolator(new Interpolator() {
+                                @Override
+                                protected double curve(double t) {
+                                        return 1.5 * t * t;
+                                }
+                        });  
+                        trans.play();
+                        */
+            }
+        });
+    }
+ 
+   
+    private void addMouseEventHandler(){
+        root.onMouseClickedProperty().set(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+            	//Play MP3 File (Flap Sound)
+				String url = getClass().getResource("/flap.mp3").toString();
+				Media m = new Media(url);
+				MediaPlayer player = new MediaPlayer(m);
+				player.play();
+				
+                //To click the screen should lift bird
+                Timeline tl = new Timeline();
+                tl.setCycleCount(1);
+                KeyValue kv = new KeyValue(flappy.translateYProperty(), -1000, new Interpolator() {
+                        @Override
+                                protected double curve(double t) {
+                                        return .75 * t - 1.5 * t * t;
+                                }
+                });
+                KeyFrame flap = new KeyFrame(new Duration(3000), kv);
+                tl.getKeyFrames().add(flap);
+                tl.setAutoReverse(false);
+                tl.play();
+               
+                /*
+                TranslateTransition trans = new TranslateTransition(new Duration(3000), flappy);
+                trans.setByY(-1000);
+                trans.setCycleCount(1);
+                trans.setInterpolator(new Interpolator() {
+                                @Override
+                                protected double curve(double t) {
+                                        //acceleration = 1.5 (same as initial drop)
+                                        return .9 * t - 1.5 * t * t;
+                                }
+                        });  
+               
+                        trans.play();
+                */
+
             }
         });
     }
     
-    private void addMouseEventHandler(){
-    	root.onMouseClickedProperty().set(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-            	
-            	//To click the screen should lift bird 
-            	        	
-            }
-        });
-    }	
-	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		//Create a Group 
