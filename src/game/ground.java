03d8ec8721efe6839183525e6b2e83bfdab2ab68
assyrian7 <bad.alien7@gmail.com>
2015-05-21 13:17:18 -0700

    Update ground.java

--- src/game/ground.java
+++ src/game/ground.java
@@ -1,3 +1,12 @@
-public class Ground{
+public class Ground extends Sprite{
+      
+    public Ground(int x, int y, int width, int height, String img){
+        super(x, y, width, height, img);
+    }
+    
+    public void update(){
+      
+    }
+    
   
 }
