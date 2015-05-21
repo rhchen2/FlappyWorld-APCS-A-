b29d6bcf8c3f32e1a056470693ff7f314e2841cb
assyrian7 <bad.alien7@gmail.com>
2015-05-21 08:05:20 -0700

    Done

--- src/game/Main.java
+++ src/game/Main.java
@@ -21,41 +21,17 @@ import javafx.util.Duration;
 
 public class Main extends Application{
 	
-	
+	private FlappyBird game = new FlappyBird(60, "Flappy Bird");
     
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		//Create a Group 
-		root = new Group();
-
-		bkgrd = new ImageView("background.png");
-		
-		flappy = new ImageView("flappy.png");
-		flappy.layoutXProperty().set(150);
-		flappy.layoutYProperty().set(50);
-		
-		button = new Button("Start");
-        button.layoutXProperty().set(150);
-		
-		
-		//Add controls
-		root.getChildren().add( bkgrd );
-		root.getChildren().add( flappy );
-		root.getChildren().add( button );
-
-		addActionEventHandler();
-
-		addMouseEventHandler();
-		
-		//Create scene and add to stage
-		Scene scene = new Scene(root, 400, 400);
-		primaryStage.setScene(scene);
+		game.initialize(primaryStage);
+		game.beginGameLoop();
 		primaryStage.show();
-		
 	}
 
 	public static void main(String[] args) {
-		Application.launch(args);
+		launch(args);
 	}
 
 }
