a361691a31cb937da48272b956419786af89c12a
assyrian7 <bad.alien7@gmail.com>
2015-05-21 08:02:27 -0700

    Code is moved

--- src/game/FlappyBird.java
+++ src/game/FlappyBird.java
@@ -15,17 +15,99 @@ import javafx.stage.Stage;
 
 public class FlappyBird extends GameWorld{
 	
-	private Sprite bird;
+	private Bird bird;
 	private MediaPlayer flap;
 	
 	public FlappyBird(int fps, String title) {
 		super(fps, title);
 	}
+	private Button button = null;
+	private Group root = null;
+	private ImageView bkgrd = null ;
+	private Node flappy = null;
+	
+	private void addActionEventHandler(){
+        button.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                //TODO: start the drop animation of the bird
+               
+                Timeline tl = new Timeline();
+                tl.setCycleCount(1);
+                KeyValue kv = new KeyValue(flappy.translateYProperty(), 1000, new Interpolator() {
+                        @Override
+                                protected double curve(double t) {
+                                return 1.5 * t * t;
+                                }
+                });
+                KeyFrame drop = new KeyFrame(new Duration(2750), kv);
+                tl.getKeyFrames().add(drop);
+                tl.setAutoReverse(false);
+                tl.play();
+               
+                /*
+                TranslateTransition trans = new TranslateTransition(new Duration(3000), flappy);
+                trans.setToY(1000);
+                trans.setCycleCount(1);                
+                trans.setInterpolator(new Interpolator() {
+                                @Override
+                                protected double curve(double t) {
+                                        return 1.5 * t * t;
+                                }
+                        });  
+                        trans.play();
+                        */
+            }
+        });
+    }
+ 
+   
+    private void addMouseEventHandler(){
+        root.onMouseClickedProperty().set(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+            	//Play MP3 File (Flap Sound)
+				String url = getClass().getResource("/flap.mp3").toString();
+				Media m = new Media(url);
+				MediaPlayer player = new MediaPlayer(m);
+				player.play();
+				
+                //To click the screen should lift bird
+                Timeline tl = new Timeline();
+                tl.setCycleCount(1);
+                KeyValue kv = new KeyValue(flappy.translateYProperty(), -1000, new Interpolator() {
+                        @Override
+                                protected double curve(double t) {
+                                        return .75 * t - 1.5 * t * t;
+                                }
+                });
+                KeyFrame flap = new KeyFrame(new Duration(3000), kv);
+                tl.getKeyFrames().add(flap);
+                tl.setAutoReverse(false);
+                tl.play();
+               
+                /*
+                TranslateTransition trans = new TranslateTransition(new Duration(3000), flappy);
+                trans.setByY(-1000);
+                trans.setCycleCount(1);
+                trans.setInterpolator(new Interpolator() {
+                                @Override
+                                protected double curve(double t) {
+                                        //acceleration = 1.5 (same as initial drop)
+                                        return .9 * t - 1.5 * t * t;
+                                }
+                        });  
+               
+                        trans.play();
+                */
 
+            }
+        });
+    }
 	public void initialize(Stage primaryStage){
 		
-		Group root = new Group();
-		ImageView bkg = new ImageView("background.png");
+		root = new Group();
+		bkg = new ImageView("background.png");
 		bird = new Bird(50, 50, 34, 24, "flappy.png");
 		String url = getClass().getResource("/flap.mp3").toString();
 		final Media media = new Media(url);
