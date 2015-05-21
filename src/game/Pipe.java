a94a37917d2ab93897d631dc2e4ee9021e035137
hellothere890 <ryanchen1996@yahoo.com>
2015-05-21 08:51:34 -0700

    updated with new methods
    created: constructor, update and getNode method.

--- src/game/Pipe.java
+++ src/game/Pipe.java
@@ -1,5 +1,15 @@
 package game;
 
 public class Pipe extends Sprite {
-
+    public Pipe(){
+      
+    }
+    public void update(){
+    //checking for boundaries
+    //update the object
+    
+    }
+    public Node getNode(){
+      return node;
+    }
 }
