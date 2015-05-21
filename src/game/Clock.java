4d6970e849d34a26c75ee995e3d07c3948f6e883
assyrian7 <bad.alien7@gmail.com>
2015-05-21 14:39:07 -0700

    Adding node to clock

--- src/game/Clock.java
+++ src/game/Clock.java
@@ -5,9 +5,12 @@ public class Clock extends Entity{
     //the node in this class will be a Text
     
     private long startTime;
+    private Text text;
   
     public Clock(){
         startTime = System.currentTimeMillis();
+        text = new Text();
+        node = text;
     }
   
     public void update(){
